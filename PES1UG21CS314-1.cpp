c63acf61d6f76322c4ed1eca9debc1e6d62018e0
Madhu12042003 <93150370+Madhu12042003@users.noreply.github.com>
2024-03-08 22:50:14 +0530

    Add files via upload

---
+++ PES1UG21CS314-1.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, I am PES1UG21CS314\n";
+  cout << "Pipeline successful\n";
+  return 0;
+}
